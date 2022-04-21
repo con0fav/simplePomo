d34e0966e5a52861d383082b052ca572b583410f
con0fav <connor.favero@gmail.com>
2022-04-20 19:01:48 -0700

    rewriting timer function

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
 //declaring variables for timer, creating basic timer function
-
-let seconds = 0;
-let interval ;
-
-function pomodoro(mins) {
-   seconds = mins*60 || 0;     
-   interval = setInterval(function() {
- 
-        seconds--;
-        if(!seconds){
-             clearInterval(interval); 
-             alert("🚨 It is Cool 😎. I wish you could share ");
-        }
-   },1000)
-};
-
-//starter code from https://levelup.gitconnected.com/creating-a-pomodoro-timer-in-javascript-in-10-lines-of-code-868f577be63c
